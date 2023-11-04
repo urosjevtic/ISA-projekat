3eedd1c93421788406788675735523ca368f6a00
Uros Jevtic <uros.jevtic888@gmail.com>
2023-11-04 14:47:16 +0100

    Initial commit

---
+++ medical-system-be/src/main/java/com/e2/medicalsystem/MedicalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.e2.medicalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalSystemApplication.class, args);
+	}
+
+}

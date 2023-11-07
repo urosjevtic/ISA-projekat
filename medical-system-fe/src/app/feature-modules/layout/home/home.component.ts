64818f0b705758e218a6b63c28ec5808f8fdc5ca
Uros Jevtic <uros.jevtic888@gmail.com>
2023-11-07 18:10:32 +0100

    showing companies on home page

--- medical-system-fe/src/app/feature-modules/layout/home/home.component.ts
+++ medical-system-fe/src/app/feature-modules/layout/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Company } from '../model/company.model';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+company1: Company = {
+  name: 'Kompanija1',
+  description: 'Opis kompanije',
+  companyLogo: ''
+};
+
+company3: Company = {
+  name: 'Kompanija3',
+  description: 'Opis trece kompanije',
+  companyLogo: ''
+};
+
+company2: Company = {
+  name: 'Kompanija2',
+  description: 'Opis kompanije asdkja lskdja lskdjalk sjdla jsdklaj lsdkja lsdj kalsjdkl ajdslaj fkasljf sdhf kasdhf kahdsfklash fdkasjldfk hfksdhf ksjdf jksadf',
+  companyLogo: ''
+};
+
+company4: Company = {
+  name: 'Kompanija4',
+  description: 'Opis kompanije asdkja lskdja lskdjalk sjdla jsdklaj lsdkja lsdj kalsjdkl ajdslaj fkasljf sdhf kasdhf kahdsfklash fdkasjldfk hfksdhf ksjdf jksadf',
+  companyLogo: ''
+};
+companies: Company[] = [this.company1, this.company2, this.company3, this.company4, this.company4, this.company4]
+
+currentIndex: number = 0;
+
+// Function to go to the previous slide
+prevSlide() {
+  if (this.currentIndex > 0) {
+    this.currentIndex--;
+  }
+}
+
+// Function to go to the next slide
+nextSlide() {
+  if (this.currentIndex < this.companies.length - 3) {
+    this.currentIndex++;
+  }
+}
 }

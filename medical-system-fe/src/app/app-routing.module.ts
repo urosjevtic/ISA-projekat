006bf8805cbc0187f98722148ec08659e87683fd
Filip Đokić <filipdjokic2001@gmail.com>
2023-11-11 13:19:29 +0100

    fix: Small fix.

--- medical-system-fe/src/app/app-routing.module.ts
+++ medical-system-fe/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './feature-modules/layout/home/home.component';
 import { AllCompaniesComponent } from './feature-modules/layout/all-companies/all-companies.component';
 import { CompanyComponent } from './feature-modules/profiles/company/company.component';
+import { RegisterComponent } from './feature-modules/layout/register/register.component';
+import { LoginComponent } from './feature-modules/layout/login/login.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
--- medical-system-fe/src/app/app.module.ts
+++ medical-system-fe/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './feature-modules/layout/layout.module';
 import { HttpClientModule } from '@angular/common/http';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+//import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [

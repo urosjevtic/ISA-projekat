5129434c515bf659fc6049bcbd8e8b062b6c92f2
Uros Jevtic <uros.jevtic888@gmail.com>
2023-11-18 21:29:00 +0100

    feat: small changes and data validations

--- medical-system-be/src/main/java/com/e2/medicalsystem/repository/CountryRepository.java
+++ medical-system-be/src/main/java/com/e2/medicalsystem/repository/CountryRepository.java
@@ -1,8 +1,7 @@
 package com.e2.medicalsystem.repository;
 
 import com.e2.medicalsystem.model.Country;
-import com.e2.medicalsystem.model.Location;
-import com.e2.medicalsystem.model.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- medical-system-fe/src/app/infrastructure/auth/register/register.component.ts
+++ medical-system-fe/src/app/infrastructure/auth/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { City, Country, RegistrationInfo } from '../model/registrationInfo.model';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -16,17 +17,17 @@ export class RegisterComponent {
     country: '',
     cities: []
   };
-  constructor(private service: AuthService){
+  constructor(private service: AuthService, private router: Router){
 
   }
   registrationForm = new FormGroup({
-    email: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     username: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required]),
     confirmPassword: new FormControl('', [Validators.required]),
     name: new FormControl('', [Validators.required]),
     surname: new FormControl('', [Validators.required]),
-    phone: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required, Validators.pattern('[0-9]{10}')]),
     profession: new FormControl(''),
 
     companyName:new FormControl('', [Validators.required]),
@@ -57,29 +58,37 @@ export class RegisterComponent {
 
   onRegisterClick()
   {
-    if(this.registrationForm.value.password === this.registrationForm.value.confirmPassword)
-    {
-      this.passwordsMatch = true;
-      const registrationInfo: RegistrationInfo={
-        email: this.registrationForm.value.email || '',
-        username: this.registrationForm.value.username || '',
-        password: this.registrationForm.value.password || '',
-        confirmPassword: this.registrationForm.value.confirmPassword || '',
-        name: this.registrationForm.value.name || '',
-        surname: this.registrationForm.value.surname || '',
-        phone: this.registrationForm.value.phone || '',
-        profession: this.registrationForm.value.profession || '',
-        companyName: this.registrationForm.value.companyName || '',
-        country: this.registrationForm.value.country || '',
-        city: this.registrationForm.value.city || '',
-        address: this.registrationForm.value.address || '',
+    if(this.registrationForm.valid){
+      if(this.registrationForm.value.password === this.registrationForm.value.confirmPassword)
+      {
+        this.passwordsMatch = true;
+        const registrationInfo: RegistrationInfo={
+          email: this.registrationForm.value.email || '',
+          username: this.registrationForm.value.username || '',
+          password: this.registrationForm.value.password || '',
+          confirmPassword: this.registrationForm.value.confirmPassword || '',
+          name: this.registrationForm.value.name || '',
+          surname: this.registrationForm.value.surname || '',
+          phone: this.registrationForm.value.phone || '',
+          profession: this.registrationForm.value.profession || '',
+          companyName: this.registrationForm.value.companyName || '',
+          country: this.registrationForm.value.country || '',
+          city: this.registrationForm.value.city || '',
+          address: this.registrationForm.value.address || '',
+        }
+  
+        this.service.registerUser(registrationInfo).subscribe({
+          next: ()=>{
+            window.alert(["Check your email"]);
+            this.router.navigate(['/home']);
+          }
+        });
+      }else
+      {
+        this.passwordsMatch = false;
       }
-
-      this.service.registerUser(registrationInfo).subscribe({
-      });
-    }else
-    {
-      this.passwordsMatch = false;
+    }else{
+      console.log("Invalid data");
     }
 
   

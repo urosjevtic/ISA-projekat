2e42a8cb9449841559ebcea2dbf8984033588a47
fik01 <114762591+fik01@users.noreply.github.com>
2023-12-17 16:58:26 +0100

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -10,7 +10,7 @@ INSERT INTO public.medical_equipment(
 	id, company_id, description, name)
 	VALUES (-1, -1, 'Vitamin D', 'Vitamin D'),
            (-2, -1, 'Amoxicillin', 'Amoxicillin'),
-           (-3, -1, 'Levothyroxine Sodium', 'Levothyroxine Sodium'),
+           (-3, -1, 'Levothyroxine', 'Levothyroxine'),
            (-4, -1, 'Lisinopril', 'Lisinopril'),
            (-5, -1, 'Ibuprofen', 'Ibuprofen'),
            (-6, -2, 'Gabapentin', 'Gabapentin'),
@@ -58,10 +58,24 @@ INSERT INTO public.users(
 	id, address, city, company_name, country, email, enabled, name, password, penal_points, phone, profession, role, surname, username)
 	VALUES (-1, 'Address1', 'Boston', 'UnitedHealth Group', 'USA', 'filipdjokic2001@gmail.com', TRUE, 'user1', '$2a$10$ddmk2X3.TMlr4aWVlVzYd.gX/KKjY8TfPAb5sKb.FCWklcWTqhn1S', 1, 6113133130, 'profession1', 0, 'user1', 'user1'),
            (-2, 'Address2', 'Toronto', 'CVS Health', 'Canada', 'filipdjokic2001@gmail.com', TRUE, 'user2', '$2a$10$ddmk2X3.TMlr4aWVlVzYd.gX/KKjY8TfPAb5sKb.FCWklcWTqhn1S', 2, 6113133130, 'profession2', 1, 'user2', 'user2'),
-           (-3, 'Address3', 'Ottawa', 'McKesson', 'Mexico', 'filipdjokic2001@gmail.com', TRUE, 'user3', '$2a$10$ddmk2X3.TMlr4aWVlVzYd.gX/KKjY8TfPAb5sKb.FCWklcWTqhn1S', 3, 6113133130, 'profession3', 2, 'user3', 'user3');
+           (-3, 'Address3', 'Ottawa', 'McKesson', 'Mexico', 'filipdjokic2001@gmail.com', TRUE, 'user3', '$2a$10$ddmk2X3.TMlr4aWVlVzYd.gX/KKjY8TfPAb5sKb.FCWklcWTqhn1S', 3, 6113133130, 'profession3', 2, 'user3', 'user3'),
+           (-4, 'Address4', 'Ottawa', 'McKesson', 'Mexico', 'filipdjokic2001@gmail.com', TRUE, 'user4', '$2a$10$ddmk2X3.TMlr4aWVlVzYd.gX/KKjY8TfPAb5sKb.FCWklcWTqhn1S', 4, 6113133130, 'profession4', 2, 'user4', 'user4'),
+           (-5, 'Address5', 'Ottawa', 'McKesson', 'Mexico', 'filipdjokic2001@gmail.com', TRUE, 'user5', '$2a$10$ddmk2X3.TMlr4aWVlVzYd.gX/KKjY8TfPAb5sKb.FCWklcWTqhn1S', 5, 6113133130, 'profession5', 2, 'user5', 'user5'),
+           (-6, 'Address6', 'Ottawa', 'McKesson', 'Mexico', 'filipdjokic2001@gmail.com', TRUE, 'user6', '$2a$10$ddmk2X3.TMlr4aWVlVzYd.gX/KKjY8TfPAb5sKb.FCWklcWTqhn1S', 6, 6113133130, 'profession6', 2, 'user6', 'user6'),
+           (-7, 'Address7', 'Ottawa', 'McKesson', 'Mexico', 'filipdjokic2001@gmail.com', TRUE, 'user7', '$2a$10$ddmk2X3.TMlr4aWVlVzYd.gX/KKjY8TfPAb5sKb.FCWklcWTqhn1S', 7, 6113133130, 'profession7', 2, 'user7', 'user7'),
+           (-8, 'Address8', 'Ottawa', 'McKesson', 'Mexico', 'filipdjokic2001@gmail.com', TRUE, 'user8', '$2a$10$ddmk2X3.TMlr4aWVlVzYd.gX/KKjY8TfPAb5sKb.FCWklcWTqhn1S', 8, 6113133130, 'profession8', 2, 'user8', 'user8'),
+           (-9, 'Address9', 'Ottawa', 'McKesson', 'Mexico', 'filipdjokic2001@gmail.com', TRUE, 'user9', '$2a$10$ddmk2X3.TMlr4aWVlVzYd.gX/KKjY8TfPAb5sKb.FCWklcWTqhn1S', 9, 6113133130, 'profession9', 2, 'user9', 'user9');
 
 INSERT INTO public.appointments(
-	id, admin_id, admin_last_name, admin_name, company_id, date, duration)
-	VALUES (-1, -3, 'Jevtic', 'Uros', -1, '2023-12-20 10:00:00', 1),
-           (-2, -3, 'Boskovic', 'Marko', -1, '2023-12-21 11:00:00', 2),
-           (-3, -3, 'Djokic', 'Filip', -1, '2023-12-22 12:00:00', 3);
+	id, admin_id, admin_last_name, admin_name, company_id, date, duration, taken)
+	VALUES (-3, -3, 'Djokic', 'Filip', -1, '2023-12-22 12:00:00', 3, FALSE);
+
+INSERT INTO public.companies_company_admins(
+	company_profile_id, company_admins_id)
+	VALUES (-1, -3),
+           (-1, -4),
+           (-2, -5),
+           (-2, -6),
+           (-3, -7),
+           (-4, -8),
+           (-5, -9);

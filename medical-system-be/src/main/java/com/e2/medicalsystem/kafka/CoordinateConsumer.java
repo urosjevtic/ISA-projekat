a4b18c3608b47f12a1da1564792e6f90032f344c
Filip Djokic <filipdjokic2001@gmail.com>
2024-01-27 15:41:47 +0100

    feat: Implementing kafka topic.

--- medical-system-be/src/main/java/com/e2/medicalsystem/kafka/CoordinateConsumer.java
+++ medical-system-be/src/main/java/com/e2/medicalsystem/kafka/CoordinateConsumer.java
@@ -1,7 +1,10 @@
 package com.e2.medicalsystem.kafka;
 
+import com.e2.medicalsystem.dto.ContractDto;
 import com.e2.medicalsystem.dto.KafkaCoords;
 import com.e2.medicalsystem.dto.LatLng;
+import com.e2.medicalsystem.model.Contract;
+import com.e2.medicalsystem.service.ContractService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -17,16 +20,32 @@ public class CoordinateConsumer {
 
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
+    private ContractService contractService;
     @KafkaListener(id="simulatorListener",topics = "simulator", groupId = "simulator",
             containerFactory = "kafkaListenerContainerFactory",autoStartup = "false")
+
+    @KafkaListener(id="contractListener",topics = "contract", groupId = "contract",
+            containerFactory = "kafkaListenerContainerFactory",autoStartup = "false")
+
     public void listenSimulator(String message)
     {
-
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             KafkaCoords coords = objectMapper.readValue(message, KafkaCoords.class);
             LatLng latLng = new LatLng(coords.getLat(),coords.getLng());
             simpMessagingTemplate.convertAndSendToUser(coords.getUser(),"/queue/simulator",latLng);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void listenContract(String message) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            ContractDto contractDto = objectMapper.readValue(message, ContractDto.class);
+
+            System.out.println("Received ContractDto: " + contractDto.toString());
 
         } catch (Exception e) {
             e.printStackTrace();
